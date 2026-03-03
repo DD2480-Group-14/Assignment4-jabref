5328ca70f812ff2dddc34d1a82ab00d49f64642f
Melker Trané <melkert@kth.se>
2026-03-03 12:58:05 +0100

    removed not used import

--- jabgui/src/test/java/org/jabref/gui/importer/BookCoverFetcherTest.java
+++ jabgui/src/test/java/org/jabref/gui/importer/BookCoverFetcherTest.java
@@ -39,7 +39,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.when;
 
 class BookCoverFetcherTest {
